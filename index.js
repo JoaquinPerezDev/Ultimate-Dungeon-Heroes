7cd7b7b6975b651d663cbe9f8e51a1b55a8f18d6
LucasPerezDev <LucasPerezDev@gmail.com>
2022-04-04 20:50:00 -0400

    restructured my game logic, added decks and deck shufflers for both character class extensions(hero & enemy). Working through the battle sequence now

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ ctx.fillText('end turn button', 875, 525)
 //Create character class, which will then extend to hero and enemies. 
 
 class Character {
-    constructor(name, position, health, strength, armor) {
+    constructor(name, position, health, strength, armor, deck) {
         this.name = name;
         this.position = position;
         this.width = 50;
@@ -37,6 +37,7 @@ class Character {
         this.health = health;
         this.strength = strength;
         this.armor = armor;
+        this.deck = deck;
     }
 
     draw() {
@@ -52,26 +53,32 @@ class Character {
         this.health = this.health - damage;
     }
 
+    pullCardFromDeck() {
+        //take from the deck. All children will have a deck
+    }
+
+    useCard(cardToPlay) {
+        
+    }
 
 
 }
 
 class Card {
-    constructor(name, cardNumber, type, position) {
+    constructor(name, cardNumber) {
         this.name = name;
         this.cardNumber = cardNumber;
-        this.type = type;
-        this.position = position;
         this.width = 100;
         this.height = 150;
     }
     
-    draw() {
+    drawCardOnCanvas(x, y) {
         ctx.fillStyle = 'white';
-        ctx.fillRect (this.position.x, this.position.y, this.width, this.height);
+        ctx.fillRect (x, y, this.width, this.height);
     }
 
 }
+// 
 
 class Level {
     //We will likely need to create a level class to shift the entire screen from 
@@ -85,48 +92,44 @@ class Level {
 //found in the game. Later on these can be extended to other types.
 
 class AttackCard extends Card {
-    constructor(name, strength, cardNumber, type, position) {
-        super(name, cardNumber, type, position)
-        this.position = position;
+    constructor(name, strength, cardNumber) {
+        super(name, cardNumber)
         this.strength = strength;
-        this.width = 100;
-        this.height = 150;
+        this.type = 'attack';
     }
     
-    draw() {
-        ctx.fillStyle = 'white';
-        ctx.fillRect (this.position.x, this.position.y, this.width, this.height);
-    }
-
-    attack() {
+    use() {
         return this.strength;
+    }
 
+    draw(x, y) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect (x, y, this.width, this.height);
     }
 }
+
 class BlockCard extends Card {
-constructor(name, block, cardNumber, type, position) {
-    super(name, cardNumber, type, position) 
-    this.position = position;
-    this.block = block;
-    this.width = 100;
-    this.height = 150;
-}
+    constructor(name, block, cardNumber) {
+        super(name, cardNumber) 
+        this.block = block;
+        this.type = 'block';
+    }
 
-draw() {
-    ctx.fillStyle = 'white';
-    ctx.fillRect (this.position.x, this.position.y, this.width, this.height);
-}
+    use() {
+        return this.block;
+    }
 
-block() {
-    return this.block;
-}
+    draw(x, y) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect (x, y, this.width, this.height);
+    }
 }
 
 
 /* our hero character */
 class Hero extends Character {
-    constructor(name, position, health, strength, armor, energy) {
-        super(name, position, health, strength, armor, energy)
+    constructor(name, position, health, strength, armor, energy, deck) {
+        super(name, position, health, strength, armor, deck)
         this.energy = energy;
     }
 
@@ -155,21 +158,13 @@ class Hero extends Character {
     }
 
 }
-//Our hero!
-const player = new Hero('player', {x: 250, y: 150}, 30, 5, 0, 3)
 
 //our enemies! This can be further extended into different kinds. 
 //Once we can functionally play through one level/set of enemies and succeed in win/losing.
 
 class Enemy extends Character {
-    constructor(name, position, health, strength, armor) {
-        super(name, position, health, strength, armor)
-        this.name = name;
-        this.width = 50;
-        this.height = 100;
-        this.health = health;
-        this.strength = strength;
-        this.armor = armor;
+    constructor(name, position, health, strength, armor, deck) {
+        super(name, position, health, strength, armor, deck)
     }
 
     draw() {
@@ -189,7 +184,7 @@ class Enemy extends Character {
             return `${this.name} has been attacked for ${damage} damage!`
         }
     }
-
+/* the below methods will be attached to other card extensions once the battle system has been figured out 
     increaseArmor() {
         this.armor += 2;
     }
@@ -197,54 +192,46 @@ class Enemy extends Character {
     enrage() {
         this.strength += 2;
     }
-
+*/
 }
-
-//our skeleton badies
-const skeleton1 = new Enemy('skeleton', {x: 700, y: 200}, 22, 3, 0)
-
-const skeleton2 = new Enemy('skeleton', {x: 850, y: 200}, 22, 3, 0)
-
 
 //the cards that comprise our deck! I have included multiples of each to account
 //for every single card included in the starting deck. This may change once
 //I can figure out how to create more efficient code.
-//                               name, strength, cardNumber, type, position
-const attack1 = new AttackCard ('Slice & dice!', 6, 1, 'attack', {x: 450, y:400})
+//                               name, strength, cardNumber
+const attack1 = new AttackCard ('Slice & dice!', 6, 1)
 
-const attack2 = new AttackCard ('Slice & dice!', 6, 2, 'attack', {x: 450, y:400})
+const attack2 = new AttackCard ('Slice & dice!', 6, 2)
 
-const attack3 = new AttackCard ('Slice & dice!', 6, 3, 'attack', {x: 450, y:400})
+const attack3 = new AttackCard ('Slice & dice!', 6, 3)
 
-const attack4 = new AttackCard ('Slice & dice!', 6, 4, 'attack', {x: 450, y:400})
+const attack4 = new AttackCard ('Slice & dice!', 6, 4)
 
-const attack5 = new AttackCard ('Slice & dice!', 6, 5, 'attack', {x: 450, y:400})
+const attack5 = new AttackCard ('Slice & dice!', 6, 5)
 
-const attack6 = new AttackCard ('Slice & dice!', 6, 6, 'attack', {x: 450, y:400})
+const attack6 = new AttackCard ('Slice & dice!', 6, 6)
 
-const block1 = new BlockCard ('Suit up!', 4, 7, 'attack', {x: 450, y:400})
+const block1 = new BlockCard ('Suit up!', 4, 7)
 
-const block2 = new BlockCard ('Suit up!', 4, 8, 'attack', {x: 450, y:400})
+const block2 = new BlockCard ('Suit up!', 4, 8)
 
-const block3 = new BlockCard ('Suit up!', 4, 9, 'attack', {x: 450, y:400})
+const block3 = new BlockCard ('Suit up!', 4, 9)
 
-const block4 = new BlockCard ('Suit up!', 4, 10, 'attack', {x: 450, y:400})
-
-
+const block4 = new BlockCard ('Suit up!', 4, 10)
 
 class Deck {
-    constructor(cards) {
-        this.cards += cards;
+    constructor() {
+        // this.cards += cards;
         this.cards = [
-            attack1, 
-            attack2, 
-            attack3, 
-            attack4, 
-            attack5, 
+            attack1,
+            attack2,
+            attack3,
+            attack4,
+            attack5,
             attack6, 
-            block1, 
-            block2, 
-            block3, 
+            block1,
+            block2,
+            block3,
             block4
         ];
     }
@@ -255,38 +242,145 @@ class Deck {
         comprised of a combination of our cards, when the game is initialized. */
     }
 
-    shuffleDeck() {
-        let shuffle;
-        for (let i = 0; i < this.cards.length; i++) {
-            shuffle = Math.floor((Math.random() * this.cards.length));
-            return shuffle;
+    shuffleDeck () {
+        let newShuffledCardsArray = this.cards;
+        let currentIndex = newShuffledCardsArray.length,  randomIndex;
+      
+        while (currentIndex != 0) {
+      
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          [newShuffledCardsArray[currentIndex], 
+           newShuffledCardsArray[randomIndex]] =
+          [newShuffledCardsArray[randomIndex], 
+           newShuffledCardsArray[currentIndex]];
+        }
+      
+        return newShuffledCardsArray;
+        console.log(newShuffledCardsArray)
+      }
         /* This function will shuffle the deck anytime we start a new level, 
         based on an event listener 'click' on 'new level' */
-    }
 
+    pullCardFromDeck(numberOfCards) {
+        let cardsDrawnArray = [];
+        for(let i = 0; i < numberOfCards; i++){
+            cardsDrawnArray.push(this.cards.shift())
+        } 
+        return cardsDrawnArray;
+    }
+  
 }
 
-    drawCards() {
-        this.shuffleDeck()
-        for(let i = 0; i < this.cards.length;i++) {
-            
+class EnemyDeck extends Deck {
+    //    constructor() {
+    //     this.cardArray = [
+    //         attack1, 
+    //         block1
+    //     ];
+
+    createDeck() {
+      return this.cards;   
+        /* This function will create a new deck, 
+        comprised of a combination of our cards, when the game is initialized. */
+    }
+
+    shuffleDeck () {
+        let newShuffledCardsArray = this.cards;
+        let currentIndex = newShuffledCardsArray.length,  randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [newShuffledCardsArray[currentIndex], 
+           newShuffledCardsArray[randomIndex]] =
+          [newShuffledCardsArray[randomIndex], 
+           newShuffledCardsArray[currentIndex]];
         }
-        /* This function will draw our */
-    }
+      
+        return newShuffledCardsArray;
+        console.log(newShuffledCardsArray)
+      }
+    // shuffleDeck() {
+    //     let shuffle;
+    //     let newShuffledCardsArray = this.cards;
+    //     for (let i = 0; i < this.cards.length; i++) {
+    //         shuffle = Math.floor((Math.random() * this.cards.length));
+    //         return shuffle;
+    //     /* This function will shuffle the deck anytime we start a new level, 
+    //     based on an event listener 'click' on 'new level' */
+    //     }
+    // }
 }
 
-const newDeck = new Deck();
+//1. create 2 decks, 1 enemy and 1 hero. the enemy should have its own deck, 
+//the hero should have its own deck(in its constructor).
+//2. for the game loop, first draw cards on screen, draw enemy and player on screen.
+//3. add eventlistener attached to mouse click relative to it's position on canvas. 
+//3A. check if eventlistener clicks on another card or end turn button, skipping 
+//when clicked it plays cards' effect on enemy and subtracts hero energy, then deletes card from hand.
+//4. create a separate event listener on end turn button, that triggers enemy to randomly select a card of its own and play it. 
+//5. After enemy plays 1 card, end enemy turn. Draw new card from player deck and re-draw all player cards on screen.
+//6. translate canvas dimensions relative to browser dimensions because mouse events are based on browser dimensions.
 
-newDeck.createDeck();
+class Battle {
+constructor(hero, enemy) {
+    this.Hero = hero;
+    this.Enemy = enemy;
+}
+// addHero() {
+//     return this.player;
+// }
 
-newDeck.shuffleDeck()
-console.log(newDeck)
+// addEnemy() {
+//     return this.skeleton1;
+// }
 
-player.draw()
-console.log(player)
+activateHeroTurn() {
+    this.Hero.deck.shuffleDeck();
+    this.Hero.hand = this.Hero.deck.pullCardFromDeck(5);
+    console.log(this.Hero.deck.pullCardFromDeck(5))
 
-skeleton1.draw()
-console.log(skeleton1)
+/* add eventListener 'mousedown'  to activate card[i] relative to where the cards 
+are positioned on screen. */
+    /* add conditional to 'mousedown' on "End Turn" button that starts enemyTurn() 
+    */
 
-skeleton2.draw()
-console.log(skeleton2)
+}
+
+activateEnemyTurn() {
+    this.Enemy.deck.shuffleDeck();
+   this.Enemy.hand = this.Enemy.deck.pullCardFromDeck(3);
+   console.log(this.Enemy.deck.pullCardFromDeck(3))
+/* add conditional to enemyTurn that ends the turn 
+once skeleton has played 1 Card from EnemyDeck. 
+*/
+}
+}
+
+//Our hero!
+const player = new Hero('player', {x: 250, y: 150}, 30, 6, 0, 3, new Deck())
+
+//our skeleton baddies
+const skeleton1 = new Enemy('skeleton', {x: 700, y: 200}, 22, 3, 0, new EnemyDeck())
+
+const skeleton2 = new Enemy('skeleton', {x: 850, y: 200}, 19, 4, 0, new EnemyDeck())
+
+
+new Battle(player, skeleton1);
+
+//1. create 2 decks, 1 enemy and 1 hero. the enemy should have its own deck, 
+//the hero should have its own deck(in its constructor).
+//2. for the game loop, first draw cards on screen, draw enemy and player on screen.
+//3. add eventlistener attached to mouse click relative to it's position on canvas. 
+//3A. check if eventlistener clicks on another card or end turn button, skipping 
+//when clicked it plays cards' effect on enemy and subtracts hero energy, then deletes card from hand.
+//4. create a separate event listener on end turn button, that triggers enemy to randomly select a card of its own and play it. 
+//5. After enemy plays 1 card, end enemy turn. Draw new card from player deck and re-draw all player cards on screen.
+//6. translate canvas dimensions relative to browser dimensions because mouse events are based on browser dimensions.
