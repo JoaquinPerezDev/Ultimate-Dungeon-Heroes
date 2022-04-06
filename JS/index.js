c7bd82c1338a64dd015bbdc57a30899d69c1475b
LucasPerezDev <LucasPerezDev@gmail.com>
2022-04-06 17:49:18 -0400

    continued refining the battle sequence

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,6 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 1024
-canvas.height = 576
-
 //Create character class, which will then extend to hero and enemies. 
 
 class Character {
@@ -39,7 +36,6 @@ class Character {
     useCard(card, target) {
         //apply card's effect here. 
         
-        console.log('apply card\'s effect and subtract energy from hero', card)
         if(card.type === 'attack') {
             target.receiveDamage(this.strength);
         }
@@ -116,22 +112,21 @@ class Hero extends Character {
         ctx.fillRect (this.position.x, this.position.y, 50, 150);
     }
 
-    gainsBlock(block) {
-        this.health = this.health + block;
-        if(this.health >= 30) {
-            return `${this.name} is becoming an impenetrable human fortress! ${this.name}'s health is ${this.health}` 
-        } else {
-            return `suit up or shut up! ${this.name} gains ${block} points of armor`
-        }
-    }
-
-
     receiveDamage(damage) {
         this.health = this.health - damage;
         if(this.health <= 0) {
-            return `The fates predicted ${this.name} would not become the Ultimate Dungeon Hero`
+            alert(`The fates predicted ${this.name} would not become the Ultimate Dungeon Hero`)
         } else {
-            return `Not the face! ${this.name} takes ${damage} points of damage. ${this.name}'s health is ${this.health}`
+            console.log(`Not the face! ${this.name} takes ${damage} points of damage. ${this.name}'s health is ${this.health}`)
+        }
+    }
+
+    gainsBlock(block) {
+        this.health = this.health + block;
+        if(this.health > 30) {
+            console.log(`${this.name} is becoming an impenetrable human fortress! ${this.name}'s health is ${this.health}`) 
+        } else {
+            console.log(`suit up or shut up! ${this.name} gains ${block} points of armor`)
         }
     }
 
@@ -140,14 +135,14 @@ class Hero extends Character {
         
         console.log(card)
         if(card.type === 'attack') {
-            target.receiveDamage(this.strength);
-            console.log(target)
+            target.receiveDamage(card.strength);
         }
         //if(card.type === 'attack') {
         // 
         if(card.type === 'block') {
-            this.gainsBlock(this.block);
-            console.log(target)
+            console.log(this.health)
+            this.gainsBlock(card.block);
+            console.log(this.health)
         }
 
         this.energy--; //this.energy -= this.cardEnergy;
@@ -159,10 +154,9 @@ class Hero extends Character {
        let remainingHand = this.hand.splice(0, this.hand.length);
         this.discardPile.push(...remainingHand);
         if(this.deck.cards.length === 0) {
-            console.log('deck length is now 0')
             this.deck.cards = this.discardPile;
             this.discardPile = [];
-            console.log('the deck has been refilled!', this.deck.cards, this.discardPile);
+            console.log('the deck has been refilled!');
         }
         this.energy = 3;
     }
@@ -182,7 +176,7 @@ class Enemy extends Character {
 
     draw() {
         ctx.fillStyle = 'orange';
-        ctx.fillRect (this.position.x, this.position.y, 50, 100);
+        ctx.fillRect (this.position.x, this.position.y, 100, 200);
     }
 
     chooseCardFromHand(cardIndexInHand) {
@@ -192,7 +186,7 @@ class Enemy extends Character {
     receiveDamage(damage) {
         this.health = this.health - damage;
         if(this.health <= 0) {
-            console.log(`our hero has slain the ${this.name}, onward!`)
+            alert(`our hero has slain the ${this.name}, onward!`)
         } else {
             console.log(`${this.name} has been attacked for ${damage} damage! ${this.name}'s health is ${this.health}`)
         } 
@@ -201,7 +195,7 @@ class Enemy extends Character {
     
     gainsBlock(block) {
         this.health = this.health + block;
-        if(this.health >= 30) {
+        if(this.health > 30) {
             console.log(`What are you doing Hero? ${this.name} is near invincible!${this.name}'s health is ${this.health}`)
         } else {
             console.log(`${this.name} is plotting your demise!`)
@@ -214,13 +208,13 @@ class Enemy extends Character {
         console.log(card)
         if(card.type === 'attack') {
             target.receiveDamage(this.strength);
-            console.log(target)
         }
         //if(card.type === 'attack') {
         // 
         if(card.type === 'block') {
+            console.log(this.health)
             this.gainsBlock(this.block);
-            console.log(target)
+            console.log(this.health)
         }
 
         this.energy--; //this.energy -= this.cardEnergy;
@@ -234,7 +228,6 @@ class Enemy extends Character {
          if(this.deck.cards.length === 0) {
             this.deck.cards = this.discardPile;
             this.discardPile = [];
-            console.log('the deck has been refilled!');
         }
      }
 /* the below methods will be attached to other card extensions once the battle system has been figured out 
@@ -274,7 +267,7 @@ const block4 = new BlockCard ('Suit up!', 4, 10)
 
 class Deck {
     constructor() {
-        // this.cards += cards;
+        //this.cards = cardArray; add (cardArray) constructor
         this.cards = [
             attack1,
             attack2,
@@ -315,7 +308,9 @@ class Deck {
 
       /* variation of code of the Fisher-Yates Shuffle. */
         /* This function will shuffle the deck anytime we start a new level, 
-        based on an event listener 'click' on 'new level' */
+        based on an event listener 'click' on 'new level' 
+        ** UPDATE 04/06/22 'new level' feature will be added in V2 or V3, \
+        once battle mechanics have been improved and new cards are introduced ** */
 
     pullCardFromDeck(numberOfCards) {
 
@@ -405,39 +400,67 @@ beginBattle() {
     this.Enemy.draw();
 }
 
-activateHeroTurn() {
-    console.log(`prepare yourself for battle, Hero!`)
 
+activateHeroTurn() {
+
+    //add text box with this floating text below. Need to look into canvas animation
+    console.log(`prepare yourself for battle, Hero!`)
+    //Need to write a function that will check each card from deck, and draw it on the board at each card position #1-5
     this.Hero.hand = this.Hero.deck.pullCardFromDeck(5);
     console.log(this.Hero.hand);
 
-    let chosenCard = this.Hero.chooseCardFromHand(3);
-    console.log(this.Hero.hand, chosenCard, this.Hero.energy);
 
-    this.Hero.useCard(chosenCard, this.Enemy);
-    console.log(this.Hero.hand, this.Hero.discardPile, this.Hero.energy);
-
-    chosenCard = this.Hero.chooseCardFromHand(1); 
-    this.Hero.useCard(chosenCard, this.Enemy);
-    console.log(this.Hero.hand, this.Hero.discardPile, this.Hero.energy); 
-
-    chosenCard = this.Hero.chooseCardFromHand(2);
-    this.Hero.useCard(chosenCard, this.Enemy);
-    console.log(this.Hero.hand, this.Hero.discardPile, this.Hero.energy);
-
-    this.Hero.endTurn();
-    console.log(this.Hero.deck, this.Hero.hand, this.Hero.discardPile);
-
-/* add eventListener 'mousedown'  to activate card[i] relative to where the cards 
-are positioned on screen. */
-    /* add conditional to 'mousedown' on "End Turn" button that starts enemyTurn() 
-    */
+    window.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case '1':
+                if(this.Hero.hand.length >= 1 && this.Hero.energy > 0) {
+                const chosenCard0 = this.Hero.chooseCardFromHand(0);
+                this.Hero.useCard(chosenCard0, this.Enemy);
+                console.log(this.Hero.hand, chosenCard0, this.Hero.discardPile, this.Hero.energy);
+                }
+                break;
+            case '2':
+                if(this.Hero.hand.length >= 2 && this.Hero.energy > 0) {
+                const chosenCard1 = this.Hero.chooseCardFromHand(1);
+                this.Hero.useCard(chosenCard1, this.Enemy);
+                console.log(this.Hero.hand, chosenCard1, this.Hero.discardPile, this.Hero.energy);
+                }
+                break;
+            case '3':
+                if(this.Hero.hand.length >= 3 && this.Hero.energy > 0) {
+                const chosenCard2 = this.Hero.chooseCardFromHand(2);
+                this.Hero.useCard(chosenCard2, this.Enemy);
+                console.log(this.Hero.hand, chosenCard2, this.Hero.discardPile, this.Hero.energy);
+                }
+                break;
+            case '4':
+                if(this.Hero.hand.length >= 4 && this.Hero.energy > 0) {
+                const chosenCard3 = this.Hero.chooseCardFromHand(3);
+                this.Hero.useCard(chosenCard3, this.Enemy);
+                console.log(this.Hero.hand, chosenCard3, this.Hero.discardPile, this.Hero.energy);
+                }
+                break;
+            case '5':
+                if(this.Hero.hand.length >= 5 && this.Hero.energy > 0) {
+                const chosenCard4 = this.Hero.chooseCardFromHand(4);
+                this.Hero.useCard(chosenCard4, this.Enemy);
+                console.log(this.Hero.hand, chosenCard4, this.Hero.discardPile, this.Hero.energy);
+                }
+                break;
+            case ' ':
+                this.Hero.endTurn();
+                this.activateEnemyTurn();
+                break;  
+        }
+      })
+    //Event listeners can be made for each card in the hand, corresponding to a number key 1-5. 
+    //A variable for each can be made for sake of simplicity
 }
 
-activateEnemyTurn() {
+activateEnemyTurn(){
     console.log(`it is the enemy's turn to act!`)
    this.Enemy.deck.shuffleDeck();
-
+//Enemy turn is fully automated, can be attached to endTurn() event listener for Hero's turn.
    this.Enemy.hand = this.Enemy.deck.pullCardFromDeck(5);
    console.log(this.Enemy.hand)
    //
@@ -447,37 +470,64 @@ activateEnemyTurn() {
    this.Enemy.useCard(chosenEnemyCard, this.Hero);
    this.Enemy.endTurn();
    console.log(this.Enemy.hand, this.Enemy.discardPile);
-
-
+ //  this.activateHeroTurn();
 /* add conditional to enemyTurn that ends the turn 
 once skeleton has played 1 Card from EnemyDeck. 
 */
-
 }
 
+// endBattle() {
+//     //add alert that game 
+// }
+
 }
+//Battle sequence function invoking and event listeners below here
 
-//Our hero!
-const player = new Hero('player', {x: 250, y: 150}, 30, 6, 0, 3, new Deck(), 0)
-//our skeleton baddies
-const skeleton1 = new Enemy('skeleton', {x: 700, y: 200}, 22, 3, 0, new Deck(), 0)
 
-const skeleton2 = new Enemy('skeleton', {x: 850, y: 200}, 19, 4, 0, new Deck(), 0)
-//our battle sequence
-let newBattle = new Battle(player, skeleton1, skeleton2);
-// newBattle.activateHeroTurn();
-// newBattle.activateHeroTurn();
-player.draw()
-skeleton1.draw()
-skeleton2.draw()
-newBattle.activateHeroTurn()
-newBattle.activateEnemyTurn()
+let newBattle;
+const startGameButton = document.getElementById('start-game-button');
+// const endTurnButton = document.getElementById('end-turn-button');
 
-newBattle.activateHeroTurn()
- newBattle.activateEnemyTurn()
-newBattle.activateHeroTurn()
-newBattle.activateEnemyTurn()
+if (startGameButton) {
+    startGameButton.addEventListener('click', event => {
 
+        canvas.width = 1024
+        canvas.height = 576
+
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        ctx.fillStyle = 'white';
+        ctx.fillText('UI PLACEMENT LEGEND', 50, 50)
+        ctx.fillText('ULTIMATE DUNGEON HERO ATTEMPT #1', 415, 50)
+        ctx.fillText('battle screen', 450, 100)
+        ctx.fillText('Hero', 250, 325)
+        ctx.fillText('hero health bar', 250, 375)
+        ctx.fillText('hero energy', 50, 425)
+        ctx.fillText('enemies', 800, 350)
+        ctx.fillText('enemy 1 turn intention', 750, 125)
+        ctx.fillText('enemy 1 health bar', 750, 375)
+        ctx.fillText('deck pile icon', 50, 475)
+        ctx.fillText('card1', 250, 500)
+        ctx.fillText('card2', 375, 500)
+        ctx.fillText('card3', 500, 500)
+        ctx.fillText('card4', 625, 500)
+        ctx.fillText('card5', 750, 500)
+        ctx.fillText('discard pile icon', 50, 525)
+        ctx.fillText('end turn button', 900, 525)
+
+
+
+        const player = new Hero('player', {x: 250, y: 200}, 30, 6, 0, 3, new Deck(), 0);
+        //our skeleton baddies
+        const skeleton1 = new Enemy('skeleton', {x: 750, y: 150}, 22, 3, 0, new Deck(), 0);
+        let newBattle = new Battle(player, skeleton1);
+        player.draw();
+        skeleton1.draw();
+        newBattle.beginBattle();
+        newBattle.activateHeroTurn();
+
+
+    });
+  }
 
 //2. for the game loop, first draw cards on screen, draw enemy and player on screen.
 //3. add eventlistener attached to mouse click relative to it's position on canvas. 
@@ -486,4 +536,6 @@ newBattle.activateEnemyTurn()
 //4. create a separate event listener on end turn button, that triggers enemy to randomly select a card of its own and play it. 
 //5. After enemy plays 1 card, end enemy turn. Draw new card from player deck and re-draw all player cards on screen.
 //6. translate canvas dimensions relative to browser dimensions because mouse events are based on browser dimensions.
+
+
 
