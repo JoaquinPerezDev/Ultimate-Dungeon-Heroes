88aae294f6797e8b9238a2cf77ced0fa2c759267
LucasPerezDev <LucasPerezDev@gmail.com>
2022-04-05 22:35:54 -0400

    completed v1 game logic, succeeding in getting the battle sequence to work as intended

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ ctx.fillText('end turn button', 875, 525)
 //Create character class, which will then extend to hero and enemies. 
 
 class Character {
-    constructor(name, position, health, strength, armor, deck) {
+    constructor(name, position, health, strength, armor, deck, block) {
         this.name = name;
         this.position = position;
         this.width = 50;
@@ -37,6 +37,7 @@ class Character {
         this.health = health;
         this.strength = strength;
         this.armor = armor;
+        this.block = block;
         this.deck = deck;
     }
 
@@ -45,23 +46,40 @@ class Character {
         ctx.fillRect (this.position.x, this.position.y, this.width, this.height);
     }
 
-    attack() {
-        return this.strength;
-    }
-
-    useAttackCard() {
-        return this.strength;
+    chooseCardFromHand(cardIndexInHand) {
+        return this.hand.splice(cardIndexInHand, 1)[0];
     }
 
     receiveDamage(damage) {
         this.health = this.health - damage;
     }
+
+    gainsBlock(block) {
+        this.health = this.health + block;
+    }
+
+    useCard(card, target) {
+        //apply card's effect here. 
+        
+        console.log('apply card\'s effect and subtract energy from hero', card)
+        if(card.type === 'attack') {
+            target.receiveDamage(this.strength);
+        }
+        //if(card.type === 'attack') {
+        // 
+        if(card.type === 'block') {
+            this.gainsBlock(this.block);
+        }
+
+        this.energy--; //this.energy -= this.cardEnergy;
+        this.discardPile.push(card);
+
+    }
 }
 
 class Card {
-    constructor(name, cardNumber) {
+    constructor(name) {
         this.name = name;
-        this.cardNumber = cardNumber;
         this.width = 100;
         this.height = 150;
     }
@@ -86,8 +104,8 @@ class Level {
 //found in the game. Later on these can be extended to other types.
 
 class AttackCard extends Card {
-    constructor(name, strength, cardNumber) {
-        super(name, cardNumber)
+    constructor(name, strength) {
+        super(name)
         this.strength = strength;
         this.type = 'attack';
     }
@@ -99,14 +117,10 @@ class AttackCard extends Card {
 }
 
 class BlockCard extends Card {
-    constructor(name, block, cardNumber) {
-        super(name, cardNumber) 
+    constructor(name, block) {
+        super(name) 
         this.block = block;
         this.type = 'block';
-    }
-
-    useBlockCard() {
-        return this.block;
     }
 
     draw(x, y) {
@@ -119,7 +133,7 @@ class BlockCard extends Card {
 /* our hero character */
 class Hero extends Character {
     constructor(name, position, health, strength, armor, energy, deck, block) {
-        super(name, position, health, strength, armor, deck)
+        super(name, position, health, strength, armor, deck, block)
         this.energy = energy;
         this.discardPile = [];
         this.hand = [];
@@ -134,9 +148,9 @@ class Hero extends Character {
     gainsBlock(block) {
         this.health = this.health + block;
         if(this.health >= 30) {
-            return `${this.Hero.name} is becoming an impenetrable human fortress!` 
+            return `${this.name} is becoming an impenetrable human fortress! ${this.name}'s health is ${this.health}` 
         } else {
-            return `suit up or shut up! ${this.Hero.name} gains ${block} points of armor`
+            return `suit up or shut up! ${this.name} gains ${block} points of armor`
         }
     }
 
@@ -144,24 +158,27 @@ class Hero extends Character {
     receiveDamage(damage) {
         this.health = this.health - damage;
         if(this.health <= 0) {
-            return `The fates predicted ${this.Hero.name} would not become the Ultimate Dungeon Hero`
+            return `The fates predicted ${this.name} would not become the Ultimate Dungeon Hero`
         } else {
-            return `Not the face! ${this.Hero.name} takes ${damage} points of damage`
+            return `Not the face! ${this.name} takes ${damage} points of damage. ${this.name}'s health is ${this.health}`
         }
     }
 
-    chooseCardFromHand(cardIndexInHand) {
-        return this.hand.splice(cardIndexInHand, 1)[0];
-    }
-
-    useCard(card) {
+    useCard(card, target) {
         //apply card's effect here. 
-        console.log('apply card\'s effect and subtract energy from hero', card)
-        // if(card === Hero.deck.AttackCard) {
-        //     this.Enemy.receiveDamage(this.AttackCard.strength);
-        // } else {
-        //     this.Hero.gainsBlock(this.BlockCard.block);
-        // }
+        
+        console.log(card)
+        if(card.type === 'attack') {
+            target.receiveDamage(this.strength);
+            console.log(target)
+        }
+        //if(card.type === 'attack') {
+        // 
+        if(card.type === 'block') {
+            this.gainsBlock(this.block);
+            console.log(target)
+        }
+
         this.energy--; //this.energy -= this.cardEnergy;
         this.discardPile.push(card);
 
@@ -170,8 +187,11 @@ class Hero extends Character {
     endTurn() {
        let remainingHand = this.hand.splice(0, this.hand.length);
         this.discardPile.push(...remainingHand);
-        if(this.deck.length === 0) {
-            this.deck.push(this.discardPile);
+        if(this.deck.cards.length === 0) {
+            console.log('deck length is now 0')
+            this.deck.cards = this.discardPile;
+            this.discardPile = [];
+            console.log('the deck has been refilled!', this.deck.cards, this.discardPile);
         }
         this.energy = 3;
     }
@@ -181,7 +201,7 @@ class Hero extends Character {
 //our enemies! This can be further extended into different kinds. 
 //Once we can functionally play through one level/set of enemies and succeed in win/losing.
 
-class Enemy extends Character {
+class Enemy extends Character { 
     constructor(name, position, health, strength, armor, deck, block) {
         super(name, position, health, strength, armor, deck)
         this.block = block;
@@ -198,42 +218,50 @@ class Enemy extends Character {
         return this.hand.splice(cardIndexInHand, 1)[0];
     }
 
-    useCard(card) {
-        //apply card's effect here. 
-        console.log('apply card\'s effect and subtract energy from hero', card)
-        // if(card === Hero.deck.AttackCard) {
-        //     this.Enemy.receiveDamage(this.AttackCard.strength);
-        // } else {
-        //     this.Hero.gainsBlock(this.BlockCard.block);
-        // }
-        this.energy--; //this.energy -= this.cardEnergy;
-        this.discardPile.push(card);
-
-    }
-
     receiveDamage(damage) {
         this.health = this.health - damage;
         if(this.health <= 0) {
-            return `our hero has slain the ${this.name}, onward!`
+            console.log(`our hero has slain the ${this.name}, onward!`)
         } else {
-            return `${this.Enemy.name} has been attacked for ${damage} damage!`
+            console.log(`${this.name} has been attacked for ${damage} damage! ${this.name}'s health is ${this.health}`)
         }
     }
     
     gainsBlock(block) {
         this.health = this.health + block;
         if(this.health >= 30) {
-            return `What are you doing ${this.Hero.name}? ${this.Enemy.name} is near invincible!` 
+            console.log(`What are you doing Hero? ${this.name} is near invincible!${this.name}'s health is ${this.health}`)
         } else {
-            return `${this.Enemy.name} is plotting your demise!`
+            console.log(`${this.name} is plotting your demise!`)
         }
+    }
+
+    useCard(card, target) {
+        //apply card's effect here. 
+        
+        console.log(card)
+        if(card.type === 'attack') {
+            target.receiveDamage(this.strength);
+            console.log(target)
+        }
+        //if(card.type === 'attack') {
+        // 
+        if(card.type === 'block') {
+            this.gainsBlock(this.block);
+            console.log(target)
+        }
+
+        this.energy--; //this.energy -= this.cardEnergy;
+        this.discardPile.push(card);
+
     }
 
     endTurn() {
         let remainingHand = this.hand.splice(0, this.hand.length);
          this.discardPile.push(...remainingHand);
-         if(this.deck.length === 0) {
-            this.deck.push(this.discardPile);
+         if(this.deck.cards.length === 0) {
+            this.deck.cards = this.discardPile;
+            this.discardPile = [];
             console.log('the deck has been refilled!');
         }
      }
@@ -390,32 +418,37 @@ constructor(hero, enemy) {
     this.Hero = hero;
     this.Enemy = enemy;
     this.Hero.deck.shuffleDeck();
-    this.Enemy.deck.shuffleDeck();
 }
 // addHero() {
-//     return this.player;
+//     this.Hero.draw();
 // }
 
 // addEnemy() {
-//     return this.skeleton1;
+//     this.Enemy.draw();
 // }
 
 activateHeroTurn() {
     console.log(`prepare yourself for battle, Hero!`)
+
     this.Hero.hand = this.Hero.deck.pullCardFromDeck(5);
     console.log(this.Hero.hand);
+
     let chosenCard = this.Hero.chooseCardFromHand(3);
     console.log(this.Hero.hand, chosenCard, this.Hero.energy);
-    this.Hero.useCard(chosenCard);
+
+    this.Hero.useCard(chosenCard, this.Enemy);
     console.log(this.Hero.hand, this.Hero.discardPile, this.Hero.energy);
+
     chosenCard = this.Hero.chooseCardFromHand(1); 
-    this.Hero.useCard(chosenCard);
+    this.Hero.useCard(chosenCard, this.Enemy);
     console.log(this.Hero.hand, this.Hero.discardPile, this.Hero.energy); 
+
     chosenCard = this.Hero.chooseCardFromHand(2);
-    this.Hero.useCard(chosenCard);
-    console.log(this.Hero.hand, this.Hero.discardPile, this.Hero.energy); 
+    this.Hero.useCard(chosenCard, this.Enemy);
+    console.log(this.Hero.hand, this.Hero.discardPile, this.Hero.energy);
+
     this.Hero.endTurn();
-    console.log(this.Hero.hand, this.Hero.discardPile);
+    console.log(this.Hero.deck, this.Hero.hand, this.Hero.discardPile);
 
 /* add eventListener 'mousedown'  to activate card[i] relative to where the cards 
 are positioned on screen. */
@@ -426,12 +459,14 @@ are positioned on screen. */
 activateEnemyTurn() {
     console.log(`it is the enemy's turn to act!`)
    this.Enemy.deck.shuffleDeck();
-   this.Enemy.hand = this.Enemy.deck.pullCardFromDeck(3);
+
+   this.Enemy.hand = this.Enemy.deck.pullCardFromDeck(5);
    console.log(this.Enemy.hand)
    //
    let chosenEnemyCard = this.Enemy.chooseCardFromHand(Math.floor(Math.random() * this.Enemy.hand));
    console.log(this.Enemy.hand, chosenEnemyCard);
-   this.Enemy.useCard(chosenEnemyCard);
+
+   this.Enemy.useCard(chosenEnemyCard, this.Hero);
    this.Enemy.endTurn();
    console.log(this.Enemy.hand, this.Enemy.discardPile);
 
@@ -446,26 +481,26 @@ once skeleton has played 1 Card from EnemyDeck.
 
 //Our hero!
 const player = new Hero('player', {x: 250, y: 150}, 30, 6, 0, 3, new Deck(), 0)
-
 //our skeleton baddies
 const skeleton1 = new Enemy('skeleton', {x: 700, y: 200}, 22, 3, 0, new Deck(), 0)
 
 const skeleton2 = new Enemy('skeleton', {x: 850, y: 200}, 19, 4, 0, new Deck(), 0)
-
-
-let newBattle = new Battle(player, skeleton1);
+//our battle sequence
+let newBattle = new Battle(player, skeleton1, skeleton2);
 // newBattle.activateHeroTurn();
 // newBattle.activateHeroTurn();
-console.log(newBattle.activateHeroTurn());
-console.log(newBattle.activateEnemyTurn());
-console.log(newBattle.activateHeroTurn());
-console.log(newBattle.activateEnemyTurn());
-console.log(newBattle.activateHeroTurn());
-console.log(newBattle.activateEnemyTurn());
+player.draw()
+skeleton1.draw()
+skeleton2.draw()
+newBattle.activateHeroTurn()
+newBattle.activateEnemyTurn()
+
+// newBattle.activateHeroTurn()
+//  newBattle.activateEnemyTurn()
+// newBattle.activateHeroTurn()
+// newBattle.activateEnemyTurn()
 
 
-//1. create 2 decks, 1 enemy and 1 hero. the enemy should have its own deck, 
-//the hero should have its own deck(in its constructor).
 //2. for the game loop, first draw cards on screen, draw enemy and player on screen.
 //3. add eventlistener attached to mouse click relative to it's position on canvas. 
 //3A. check if eventlistener clicks on another card or end turn button, skipping 
